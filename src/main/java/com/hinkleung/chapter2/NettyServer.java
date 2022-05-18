77e60b658cfda6c32f075f05f0581426689987dc
qingliang.chen <394869042@qq.com>
2022-05-18 21:53:13 +0800

    server start

--- src/main/java/com/hinkleung/chapter2/NettyServer.java
+++ src/main/java/com/hinkleung/chapter2/NettyServer.java
@@ -8,6 +8,8 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 
 public class NettyServer {
 
@@ -31,5 +33,20 @@ public class NettyServer {
                     }
                 })
                 .bind(8000);
+    }
+
+    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+        serverBootstrap.bind(port)
+                .addListener(new GenericFutureListener<Future<? super Void>>() {
+                    @Override
+                    public void operationComplete(Future<? super Void> future) throws Exception {
+                        if (future.isSuccess()) {
+                            System.out.println("端口[" + port + "]绑定成功!");
+                        } else {
+                            System.err.println("端口[" + port + "]绑定失败!");
+                            bind(serverBootstrap, port + 1);
+                        }
+                    }
+                });
     }
 }
