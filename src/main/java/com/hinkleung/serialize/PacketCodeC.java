a438c44f787f3c54ad7d3870450294f634b934d4
qingliang.chen <394869042@qq.com>
2022-05-19 23:00:24 +0800

    transmission protocol definition, and encode/decode method

--- src/main/java/com/hinkleung/serialize/PacketCodeC.java
+++ src/main/java/com/hinkleung/serialize/PacketCodeC.java
@@ -55,39 +55,30 @@ public class PacketCodeC {
     public Packet decode(ByteBuf byteBuf) {
         // 跳过Magic number
         byteBuf.skipBytes(4);
-
         // 跳过版本号
         byteBuf.skipBytes(1);
-
         // 序列化算法标识
         byte serializeAlgorithm = byteBuf.readByte();
-
         // 指令
         byte command = byteBuf.readByte();
-
         // 数据包长度
         int length = byteBuf.readInt();
 
         byte[] bytes = new byte[length];
         byteBuf.readBytes(bytes);
-
         Class<? extends Packet> requestType = getRequestType(command);
         Serializer serializer = getSerializer(serializeAlgorithm);
-
         if (requestType != null && serializer != null) {
             return serializer.deserialize(requestType, bytes);
         }
-
         return null;
     }
 
     private Serializer getSerializer(byte serializeAlgorithm) {
-
         return serializerMap.get(serializeAlgorithm);
     }
 
     private Class<? extends Packet> getRequestType(byte command) {
-
         return packetTypeMap.get(command);
     }
 
